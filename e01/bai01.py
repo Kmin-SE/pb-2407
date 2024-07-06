9203706ef4a19cfc6ffc4f09e25ee1375cb50db9
baovu <vubao.edu@gmail.com>
2024-07-06 15:07:38 +0700

    e01

---
+++ e01/bai01.py
@@ -0,0 +1,10 @@
+# 1. Input - Nhập
+name = input("Nhap ten: ") # Dữ liệu nhận đc luôn là string
+age = int(input("Nhap tuoi: ")) # Nhập dữ liệu và chuyển thành int
+# age_int = int(age) # Chuyển thành kiểu int
+
+# 2. Process - Xử lý, tính toán
+year = 60 - age + 2024
+
+# 3. Output - Xuất
+print(name, "se duoc 60 tuoi vao nam", year) # Output: Xuất
---
+++ e01/bai02.py
@@ -0,0 +1,14 @@
+# 1. Input
+# Nhập tuổi vào 3 biến: dad, mom, me
+dad = int(input("Nhap tuoi cha: "))
+mom = int(input("Nhap tuoi me: "))
+me = int(input("Nhap tuoi ban: "))
+
+# 2. Process
+# Tính tuổi trung binh: tuoi_tb = (dad + mom + me) / 3
+tuoi_tb = (dad + mom + me) / 3
+
+# 3. Output
+# Xuất biến tuoi_tb
+print("Tuoi TB cua 3 nguoi la:", tuoi_tb)
+
